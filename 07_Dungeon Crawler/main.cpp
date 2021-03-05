82c634348860c7836cf2ec1b5f6eff9028b41aed
Alkoarism <Alkoarism@gmail.com>
2021-03-04 23:53:02 -0300

    Fully functional game.

--- 07_Dungeon Crawler/main.cpp
+++ 07_Dungeon Crawler/main.cpp
@@ -11,33 +11,36 @@ using std::endl;
 using std::string;
 
 int startMenu(Dungeon&);
-int gameRun(Dungeon&);
-// ---------------- Function to be created
-void configMenu()
-{ cout << "\nDon not blame me, you can play the game right?\n"; }
+int configMenu(int&, int&, int&, char&);
+int gameRun(Dungeon&, const char&);
 
 int main()
 {
     fileRead("Output.txt", "Presentation", "EndPresentation");
 
-    int gState = 0;
     char eName = 'E';
-    int rows = 10, cols = 10, traps = 10, enemys = 0;
+    int size = 10, traps = 10, enemys = 5;
     while (true)
     {
-        Dungeon game;
+        Dungeon game(size, size, traps);
+        game.initEnemys(enemys, eName);
         // ---------------- Process the menu options
         // ---------------- Game preconfig on the settings section
-        gState = startMenu(game);
+        int gState = startMenu(game);
 
-        // ---------------- The Game
-        if (gState == 1){
-            gameRun(game);
-        } else {
-            cout << "Thank you for playing my game.\n"
-                 << "Have a good life."
-                 << endl;
-            return 0;
+        switch (gState){
+            case 1:
+                gameRun(game, eName);
+            break;
+            case 0:
+                configMenu(size, traps, enemys, eName);
+            break;
+            case -1:
+                cout << "Thank you for playing my game.\n"
+                    << "Have a good life."
+                    << endl;
+                return 0;
+            break;
         }
     }
 }
@@ -69,8 +72,7 @@ int startMenu(Dungeon& game)
             break;
             case 3:
             {
-                // ---------------- Settings (too much work for now)
-                configMenu();
+                return 0;
             }
             break;
             case 4:
@@ -78,9 +80,7 @@ int startMenu(Dungeon& game)
             break;
             default:
             {
-                cerr << choice
-                     << " is not a valid choice, please try again."
-                     << endl;
+                fileRead("Output.txt", "ErrorMsg", "EndErrorMsg");
             }
             break;
             }
@@ -89,7 +89,142 @@ int startMenu(Dungeon& game)
     }
 }
 
-int gameRun(Dungeon& game){
+int configMenu
+    (int& size, int& traps, int& enemys, char& eName){
+
+    while (true){
+        fileRead("Output.txt", "ConfigTxt", "EndConfigTxt");
+
+        int choice = 0;
+        cin >> choice;
+
+        if (cin.fail()){
+            cerr << "Where did you read you should input"
+                    << " anything but a number? huh?\n"
+                    << "Try again."
+                    << endl;
+            cin.clear();
+            clearOutput();
+        } else {
+            switch(choice){
+                case 1:
+                    return 0;
+                break;
+                case 2:
+                {
+                    bool safe = false;
+                    while(!safe){
+                        cout << "\nThe number of traps is limited to you board."
+                                << "\nThe current maximun is: " << (size * size) /2
+                                << "\nInput the number of traps you want: ";
+                        cin >> traps;
+
+                        if (cin.fail()){
+                            cerr << "\nYeah. Each option have a different error message."
+                                    << "\nCongratulations, you failed on a simple task."
+                                    << endl;
+                            cin.clear();
+                        } else {
+                            if( traps > ( ( size * size ) / 2 ) || traps < 0 ){
+                                cerr << "\nPlease, could you choose a proper number?"
+                                        << endl;
+                            } else {
+                                safe = true;
+                            }
+                        }
+                        clearOutput();
+                    }
+                }
+                break;
+                case 3:
+                {
+                    bool safe = false;
+                    while(!safe){
+                        cout << "\nThe number of enemys is limited to you board."
+                                << "\nThe current maximun is: " << (size * size)/2
+                                << "\nInput the number of enemys you want: ";
+                        cin >> enemys;
+
+                        if (cin.fail()){
+                            cerr << "\nThere you go. Your error message of the day."
+                                    << "\nPlease, use the program properly ;.; ."
+                                    << endl;
+                            cin.clear();
+                        } else {
+                            if( enemys > ( ( size * size ) / 2 ) || enemys < 0){
+                                cerr << "\nI have TOLD you the range, did I not?"
+                                        << "\nDo it right this time."
+                                        << endl;
+                            } else {
+                                safe = true;
+                            }
+                        }
+                        clearOutput();
+                    }
+                }
+                break;
+                case 4:
+                {
+                    bool safe = false;
+                    while(!safe){
+                        safe = true;
+                        int temp = 0;
+                        cout << "\nInput a board size ranging from 5 to 35: ";
+                        cin >> temp;
+
+                        if (cin.fail()){
+                            cerr << "\nEaster egg in the error messages! YAY!" << endl;
+                            cin.clear();
+                        } else {
+                            if(temp >= 5 && temp <= 35){
+                                size = temp;
+                            } else {
+                                fileRead("Output.txt", "ErrorMsg", "EndErrorMsg");
+                                safe = false;
+                            }
+                        }
+                        clearOutput();
+                    }
+                } 
+                break;
+                case 5:
+                {
+                    bool safe = false;
+                    while(!safe){
+                        cout << "\nChoose the enemy tag (you can not use any repeated tag): ";
+                        cin >> eName;
+
+                        if (eName == 'P' || eName == 'T' || eName == '.' || eName == 'X'){
+                            cerr << "\nYou HAD to choose the only tags"
+                                    << "that were NOT available right?"
+                                    << "\nWell, try again, troublemaker."
+                                    << endl;
+                        } else {
+                            safe = true;
+                        }
+                        clearOutput();
+                    }
+                }
+                break;
+                case 6:
+                    size = 10;
+                    traps = 10;
+                    enemys = 5;
+                    eName = 'E';
+
+                    cout << "\nAll values have been set to default."
+                         << "\nSize = " << size
+                         << "\nTraps = " << traps
+                         << "\nEnemys = " << enemys
+                         << "\nEnemys name = " << eName
+                         << endl;
+                break;
+            }
+        }
+    }    
+}
+
+int gameRun(Dungeon& game, const char& eName){
     game.print();
     while (true)
     {
@@ -121,17 +256,16 @@ int gameRun(Dungeon& game){
             case '0':
                 return 0;
             default:
-                cerr <<"Not a valid input!\n"
-                        <<"Try harder if you want to break my program buddy.\n"
-                        <<"Something like \'w\' \'a\' \'s\' or \'d\'."
-                        << endl;
+                fileRead("Output.txt", "ErrorMsg", "EndErrorMsg");
+                cerr <<"Input something like \'w\' \'a\' \'s\' \'d\' or \'0\'."
+                     << endl;
                 safe = false;
             break;
             }
         }
 
         int victory = game.movePlayer(y, x);
-        if (game.moveEnemys('E') != 0)
+        if (game.moveEnemys(eName) != 0)
         {
             victory = -1;
         }
